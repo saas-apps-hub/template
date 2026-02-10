9ab968b1d0547c028d533bec935516817589f920
Durmuş Demirtaş <demirtasdurmus@gmail.com>
2026-02-10 11:04:20 +0300

    chore: add top level linting util scripts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,7 +1,7 @@
 import { sample } from "@template/core";
 import express from "express";
 
-const host = process.env.BACKEND_HOST ?? "localhost";
+const host = process.env.BACKEND_HOST ?? "0.0.0.0";
 const port = process.env.BACKEND_PORT ? Number(process.env.BACKEND_PORT) : 8000;
 
 const app = express();
